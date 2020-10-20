494ea7dd3fffe13fc9bcd26f5190aa3911ed2e7d
Nechitaylo Vladimir <vs_nechitaylo@bingo-boom.com>
2020-10-20 21:46:46 +0300

    Updates

---
+++ precache-manifest.5c5c999d572deb9d696e37bd4e62d42c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a751cd223236c2f581cc585f0f9aca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64f843dc582c06a12016",
+    "url": "/static/css/main.37457c82.chunk.css"
+  },
+  {
+    "revision": "cc169a5b6a8d2c869f04",
+    "url": "/static/js/2.569eae2a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.569eae2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f843dc582c06a12016",
+    "url": "/static/js/main.db3d49fc.chunk.js"
+  },
+  {
+    "revision": "c5e7b408ed1922744b94",
+    "url": "/static/js/runtime-main.e8402bce.js"
+  },
+  {
+    "revision": "09f4068bb74fc78ffbce74f5596d6aa5",
+    "url": "/static/media/Inter-Black.09f4068b.woff2"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "79bbc418ddd49af9cc493e4c6b62e39c",
+    "url": "/static/media/add-button-min.79bbc418.svg"
+  },
+  {
+    "revision": "b9b65c7e7e563b8df5ac93cc01872a72",
+    "url": "/static/media/edit-button-min.b9b65c7e.svg"
+  },
+  {
+    "revision": "7e2a03e4152feeeb376c5b6a55d7a315",
+    "url": "/static/media/like-fill-min.7e2a03e4.svg"
+  },
+  {
+    "revision": "e7a8906a3b0887219bad0d62de4f7612",
+    "url": "/static/media/like-min.e7a8906a.svg"
+  },
+  {
+    "revision": "e889e45f7898567d7ada789a8ffa0e63",
+    "url": "/static/media/logo-min.e889e45f.svg"
+  },
+  {
+    "revision": "30fadb2ec42766e17986bc3d715f7308",
+    "url": "/static/media/trash-min.30fadb2e.svg"
+  }
+]);
