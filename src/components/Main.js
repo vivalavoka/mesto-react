cfa5a7fe33ee016ab14f6dfd505e5a5da6c66ae4
Нечитайло Владимир Сергеевич <vs_nechitaylo@bingo-boom.com>
2020-10-20 15:34:37 +0300

    feature/project-10: Create Api module

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import avatar from '../images/avatar.png';
+
+import api from '../utils/api.js';
 
 export default function Main(props) {
+  const [userName, setName] = React.useState();
+  const [userDescription, setDescription] = React.useState();
+  const [userAvatar, setAvatar] = React.useState();
+
+  React.useEffect(() => {
+    api.getProfile()
+      .then(data => {
+        setName(data.name);
+        setDescription(data.about);
+        setAvatar(data.avatar);
+      })
+  });
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container">
           <div className="button button_action_pencil profile__edit-avatar" onClick={props.onEditAvatar}></div>
-          <img className="profile__avatar" src={avatar} alt="Аватар" />
+          <img className="profile__avatar" src={userAvatar} alt="Аватар" />
         </div>
         <article className="profile__info">
           <div className="profile__info-top">
-            <h1 className="profile__title">Жак-Ив Кусто</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button type="button" className="button button_action_pencil profile__edit-button" onClick={props.onEditProfile}></button>
           </div>
           <div className="profile__info-bottom">
-            <p className="profile__subtitle">Исследователь океана</p>
+            <p className="profile__subtitle">{userDescription}</p>
           </div>
         </article>
         <button type="button" className="button button_action_cross profile__add-button" onClick={props.onAddPlace}></button>
